b1d548d53dfbcfa456b5ab04329e1819bbff4c42
UGC2885 <lucas_unzer@hotmail.com>
2025-03-26 17:54:09 -0300

    Implementando um TimeoutID
    Feito a alteração para que não sobrecarrege o aplicativo e caia por muitas requisições.

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -20,15 +20,15 @@ const Search = () => {
     }), false)
 
     useEffect(() => {
-        const func = async () => {
+        const timeoutId = setTimeout( async () => {
             if (searchQuery.trim()) {
                 await loadMovies()
             } else {
                 reset()
             }
-        }
+        }, 500)
 
-        func()
+        return () => clearTimeout(timeoutId)
     }, [searchQuery])
     return (
         <View className='flex-1 bg-primary'>
